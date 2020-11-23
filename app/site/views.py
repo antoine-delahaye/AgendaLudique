7b4539dddd3c3f28c0c9396a231485d3bb5fbf24
Antoine Delahaye <antoine.delahaye24.01@gmail.com>
2020-11-23 14:43:11 +0100

    Récupération des données du catalogue depuis la base de données

--- app/site/views.py
+++ app/site/views.py
@@ -4,6 +4,8 @@ from flask import render_template
 from flask_login import login_required
 
 from . import site
+from .. import db
+from ..models import Game
 
 
 @site.route('/')
@@ -18,6 +20,11 @@ def home():
 @login_required
 def catalog():
     """
-    Render the library template on the /library route
+    Render the library template on the /catalog route
     """
-    return render_template('catalog.html', stylesheet='catalog')
+    games_data = []
+    for data in db.session.query(Game).all():
+        games_data.append(
+            {'title': data.title, 'publication_year': int(data.publication_year), 'min_players': int(data.min_players),
+             'max_players': int(data.max_players), 'image': data.image})
+    return render_template('catalog.html', stylesheet='catalog', games_data=games_data)
