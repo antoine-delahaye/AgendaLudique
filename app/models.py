b1eda03be1e4e0dd9daf496482edc90610a1a048
Antoine Delahaye <2254067-UG7z@users.noreply.gitlab.com>
2021-01-11 10:19:26 +0000

    Mise à jour de l'image de création de compte

--- app/models.py
+++ app/models.py
@@ -20,7 +20,8 @@ class User(UserMixin, db.Model):
     last_name = db.Column(db.String(32), index=True)
     password_hash = db.Column(db.String(128))
     profile_picture = db.Column(db.String(512),
-                                default='https://i.pinimg.com/originals/7d/b8/fe/7db8fe54d55fb987b2e73cbceaf22072.jpg')
+                                default='https://acsmmontreal.qc.ca/wp-content/uploads/2020/09/blank-profile-picture'
+                                        '-973460_1280-7.png')
 
     @property
     def password(self):
