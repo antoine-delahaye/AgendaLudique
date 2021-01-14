53876b2d3dbbef058a1f84a7461dc10aa13d8a2c
Florian Savouré <5079066-P0SlX@users.noreply.gitlab.com>
2021-01-14 17:29:13 +0100

    Ajout d'un nouveau script de gestion du serveur, plus stable normalement....

---
+++ refresh_server.py
@@ -0,0 +1,15 @@
+import git
+import subprocess
+import time
+
+repo = git.Repo('/home/p0slx/Github/test')
+
+while True:
+    current = repo.head.commit
+    repo.remotes.origin.pull()
+    if current != repo.head.commit:
+        print("Nouvelle version trouvée\nRedémarrage des services")
+        subprocess.run(["systemctl", "restart", "nginx"])
+        subprocess.run(["systemctl", "restart", "uwsgi.service"])
+        print("It's all good man")
+    time.sleep(5)
