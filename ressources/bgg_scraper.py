daa2c81ae6b92f49207493f46c1ee70933ead4b2
Florian Savouré <5079066-P0SlX@users.noreply.gitlab.com>
2021-01-15 18:36:51 +0100

    Aération du code pour une meilleur lisibilité

--- ressources/bgg_scraper.py
+++ ressources/bgg_scraper.py
@@ -4,8 +4,9 @@ import sys
 import mariadb
 import requests
 import yaml
-from concurrent.futures.thread import ThreadPoolExecutor
 from bs4 import BeautifulSoup
+from difflib import SequenceMatcher
+from concurrent.futures.thread import ThreadPoolExecutor
 
 domain = "https://boardgamegeek.com"
 main_url = "https://boardgamegeek.com/browse/boardgame/page/"
@@ -52,6 +53,7 @@ def get_cover(image_id):
         json_file = requests.get(cover_url + str(image_id)).json()['images']
     except TypeError:  # If we can get the webpage we got a None type and so raise a TypeError exception
         return None
+
     if "pic1657689" in json_file["original"]["url"]:
         return None
     return {
@@ -83,15 +85,18 @@ def get_game_info(url):
     # The JSON is embedded in script tag so we have to parse it
     a = map(str, beautiful_html.find_all('script'))  # Transform it to an Iterator[str]
     json_text = None
+
     for elem in a:  # For each <script>
         if 'GEEK.geekitemPreload' in elem:  # if we got the infos
             temp = elem.split('GEEK.geekitemPreload = ')[1]  # Get rid of previous code
             temp2 = temp.split('};', 1)[0]  # Get rid of useless code after the JSON file
             temp2 = temp2 + '}'  # Restore the '}' we removed the line before
             json_text = json.loads(temp2)['item']  # Transform dirty string into proper JSON format
+
     covers = get_cover(json_text["imageid"])
     if covers is None:
         return None
+
     return {
         "title": json_text["name"],
         "publication_year": int(json_text["yearpublished"]),
@@ -107,11 +112,14 @@ def get_game_info(url):
 def create_game_list(raw_html):
     game_list_dict = {}
     global i
+
     for game in map(str, raw_html.find_all("a", {"class": "primary"})):  # This way we can iterate and parse html
         game = BeautifulSoup(game, "html.parser")  # Convert str into bs4 object
         href = game.find('a')['href']  # extract href content
         id_game = int(href.split('/')[2])  # extract id from href content
+
         game_info = get_game_info(href)  # Get game info into dict
+
         if game_info is not None:  # Check if we have game info, if not skip this game
             game_list_dict[id_game] = game_info  # Transfer game info into the big ass dict
             game_list_dict[id_game]["rank"] = i  # Add rank to save order
