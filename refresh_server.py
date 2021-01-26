860be01c821def75a61f14b10c73310c31f00874
Antoine Delahaye <antoine.delahaye24.01@gmail.com>
2021-01-26 17:36:34 +0100

    Correction du script de redémarrage automatique

--- refresh_server.py
+++ refresh_server.py
@@ -6,10 +6,13 @@ repo = git.Repo('./')
 
 while True:
     current = repo.head.commit
-    repo.remotes.origin.pull()
+    try:
+        repo.remotes.origin.pull()
+    except git.GitCommandError:
+        pass
     if current != repo.head.commit:
         print("Nouvelle version trouvée\nRedémarrage des services")
         subprocess.run(["systemctl", "restart", "nginx"])
         subprocess.run(["systemctl", "restart", "uwsgi.service"])
         print("It's all good man")
-    time.sleep(5)
+    time.sleep(10)
