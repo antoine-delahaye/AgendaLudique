966bb2ecaa7839098330c6d29eb5eda0f38bfb6d
Xavier Bérault <xavier.berault@gmail.com>
2021-02-17 23:49:16 +0100

    separated models from view on group

--- app/games/group/models/group_tools.py
+++ app/games/group/models/group_tools.py
@@ -1,3 +1,6 @@
+from app.models import Game
+
+
 def get_all_participation(user):
     """
     return all groups in which the users participate ( except ones which are leaded by him)
@@ -9,3 +12,46 @@ def get_all_participation(user):
         if participation.group.manager_id != user.id:
             groups_data.append(participation.group)
     return groups_data
+
+
+def populate_games_form(form):
+    """
+    populate form data from database
+    :param form: a GamesSearchForm
+    """
+    for data in Game.all():
+        form.title.choices.append(data.title)
+        if data.publication_year not in form.years.choices:
+            form.years.choices.append(data.publication_year)
+        if data.min_players not in form.min_players.choices:
+            form.min_players.choices.append(data.min_players)
+        if data.max_players not in form.max_players.choices:
+            form.max_players.choices.append(data.max_players)
+        if data.min_playtime not in form.min_playtime.choices and data.min_playtime not in form.max_playtime.choices:
+            form.min_playtime.choices.append(data.min_playtime)
+            form.max_playtime.choices.append(data.min_playtime)
+
+
+def beautify_games_form(form):
+    """
+    sort every data of the form
+    :param form: a GamesSearchForm
+    """
+    form.years.choices.sort()
+    form.min_players.choices.sort()
+    form.max_players.choices.sort()
+    form.min_playtime.choices.sort()
+    form.max_playtime.choices.sort()
+
+
+def add_default_values_game_form(form):
+    """
+    add the "Aucun.e" value to the beginning of every choices
+    :param form: a GamesSearchForm
+    """
+    form.title.choices.insert(0, 'Aucun')
+    form.years.choices.insert(0, 'Aucune')
+    form.min_players.choices.insert(0, 'Aucun')
+    form.max_players.choices.insert(0, 'Aucun')
+    form.min_playtime.choices.insert(0, 'Aucune')
+    form.max_playtime.choices.insert(0, 'Aucune')
