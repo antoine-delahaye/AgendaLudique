227f20fae9a49798a882157266b8610c03f4087d
Amki45 <amkidelanneau@gmail.com>
2021-02-17 11:42:52 +0100

    View optimized for Users search

--- app/site/views.py
+++ app/site/views.py
@@ -29,34 +29,22 @@ def users():
     """
     form = UsersSearchForm()
     page = request.args.get('page', 1, type=int)
-    search_parameters = list()
-    qs_search_parameters = request.args.get('searchParameters', None, type=str)
 
+    # Get search parameters
+    favOnly = form.display_favorites_players_only.data if form.display_favorites_players_only.data else request.args.get('favOnly', None, type=bool)
+    hidden = form.display_masked_players.data if form.display_masked_players.data else request.args.get('hidden', None, type=bool)
+
+    # Fill search bar with parameters when changing page
     if not form.username_hint.data:
-        # Fill search bar with parameters when changing page
         form.username_hint.data = request.args.get('username', '', type=str)
-    
-    if form.validate_on_submit():
-        if form.display_masked_players.data:
-            search_parameters.append("HIDDEN")
 
-        if form.display_favorites_players_only.data:
-            search_parameters.append("ONLY_BOOKMARKED")
+    # Check the box if the parameters are True
+    form.display_favorites_players_only.data = favOnly
+    form.display_masked_players.data = hidden
 
-    if qs_search_parameters:
-        # Add the search parameters contained in the query string into the search_parameters list
-        parameters_list = qs_search_parameters.split(',')
-        for parameter in parameters_list:
-            search_parameters.append(parameter)
-            # Show in the advanced search menu the enabled parameters
-            if parameter == "ONLY_BOOKMARKED":
-                form.display_favorites_players_only.data = True
-            if parameter == "HIDDEN":
-                form.display_masked_players.data = True
+    search_results = User.search_with_pagination(current_user, form.username_hint.data, favOnly, hidden, page, 20)
 
-    search_results = User.search_with_pagination(current_user, form.username_hint.data, search_parameters, page, 20)
-
-    return render_template('users.html', stylesheet='users', form=form, current_user_id=current_user.id, users_data=search_results, search_parameters=search_parameters, username_hint=form.username_hint.data)
+    return render_template('users.html', stylesheet='users', form=form, current_user_id=current_user.id, users_data=search_results, favOnly=favOnly, hidden=hidden, username_hint=form.username_hint.data)
 
 
 @site.route('/user')
