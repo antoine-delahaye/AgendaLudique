645dc4ca62770f0e0e0296a02b2d9e4d0a147fed
Jules G <dev@julesgdn.fr>
2021-02-17 16:10:28 +0100

    Modifies the blueprint to avoid access errors to static files on the account page.

--- app/site/__init__.py
+++ app/site/__init__.py
@@ -2,6 +2,6 @@
 
 from flask import Blueprint
 
-site = Blueprint('site', __name__, template_folder="templates", static_folder="static")
+site = Blueprint('site', __name__, template_folder="templates", static_folder="static", static_url_path="/site/static")
 
 from app.site import views
