d44a020fc4f9d5a58a9d45984c03d4c5d6ac038d
Mathis Dupuis <mathis.comptegogle@gmail.com>
2021-02-18 15:50:11 +0100

    group code moved and adapted to respect MVC method

--- app/games/group/views.py
+++ app/games/group/views.py
@@ -4,7 +4,7 @@ from flask_login import login_required, current_user
 
 from . import group as gp
 from app.models import Group, Participate, User
-from .models.group_tools import get_all_participation
+from .models.group_tools import get_all_participation, join_private_group_form, join_public_group_form, quit_group_form
 from .models.forms import JoinPrivateGroupForm
 from app import db
 
@@ -15,8 +15,12 @@ def groups():
     """
     Render the groups template on the /groups route
     """
+    form = JoinPrivateGroupForm()
+    if request.method == "POST":
+        return join_private_group_form(form)
+
     groups_data = Group.query.all()
-    return render_template('groups.html', stylesheet='groups', groups_data=groups_data)
+    return render_template('groups.html', stylesheet='groups', groups_data=groups_data, form=form)
 
 
 @gp.route('/groups_public', methods=['GET', 'POST'])
@@ -27,16 +31,7 @@ def groups_public():
     """
     form = JoinPrivateGroupForm()
     if request.method == "POST":
-        code=form.code.data
-        group = Group.from_code(code)
-        if group is None:
-            flash('Ce code ne correspond à aucun groupe.', 'danger')
-        elif Participate.from_both_ids(current_user.id, group.id) is not None:
-            flash('Vous êtes déjà dans ce groupe', 'warning')
-        else:
-            db.session.add(Participate(group_id=group.id, member_id=current_user.id))
-            db.session.commit()
-            return redirect(url_for("group.group",id=group.id))
+        return join_private_group_form(form)
 
     groups_data = Group.query.filter(Group.is_private == False).all()
     return render_template('groups.html', stylesheet='groups', groups_data=groups_data, form=form)
@@ -67,44 +62,21 @@ def my_groups():
     """
     Render the groups template on the /my_groups route
     """
-    groups_data = get_all_participation(current_user)
-    return render_template('my_groups.html', stylesheet='groups', groups_data=groups_data,
+    return render_template('my_groups.html',
+                           stylesheet='groups',
+                           groups_data=get_all_participation(current_user),
                            managed_groups=list(current_user.managed_groups))
 
 
 @gp.route('/join_public_group/<group_id>', methods=['GET', 'POST'])
 @login_required
 def join_public_group(group_id=None):
-    if id is not None: # public group by id
-        group = Group.from_id(group_id)
-        if group is None:
-            abort(404)
-        elif group.is_private:
-            flash('Vous ne pouvez pas rejoindre un groupe privé sans un code.', 'danger')
-            return redirect(url_for('group.groups'))
-        else:
-            db.session.add(Participate(group_id=group_id, member_id=current_user.id))
-            db.session.commit()
-    else:
+    if id is None:
         abort(412)
-
-    return redirect(request.referrer)
+    return join_public_group_form(group_id)
 
 
 @gp.route('/quit_group/<group_id>', methods=['GET', 'POST'])
 @login_required
 def quit_group(group_id):
-    group = Group.from_id(group_id)
-    if group is None:
-        abort(412)
-    db.session.delete(Participate.from_both_ids(current_user.id, group_id))
-
-    participations = group.participations.all()
-
-    if participations == 0:  # if the group in now empty
-        db.session.delete(group)
-    elif current_user.id == group.manager_id:  # if the group doesnt have a manager anymore
-        group.manager_id = participations[0].member_id  # nominate a new manager
-
-    db.session.commit()
-    return redirect(request.referrer)
+    return quit_group_form(group_id)
