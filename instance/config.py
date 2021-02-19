98abeacee0963009a3de8587105475f7cb0e7c50
Antoine Delahaye <antoine.delahaye24.01@gmail.com>
2021-02-19 14:54:20 +0100

    Little fix

--- instance/config.py
+++ instance/config.py
@@ -2,4 +2,3 @@ import os
 
 SECRET_KEY = os.urandom(32)
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://al_admin:al_admin@agenda-ludique.ddns.net/agendaludique'
-#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://al_admin:al_admin@localhost/agendaludique'
--- run_app.py
+++ run_app.py
@@ -8,4 +8,4 @@ config_name = os.getenv('FLASK_CONFIG', 'default')
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=56733)
+    app.run()
