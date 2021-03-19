460b27bb3ecbdff707314005014599fd81005fb4
Mathis Dupuis <mathis.comptegogle@gmail.com>
2021-03-19 02:59:28 +0100

    code in organize_session() (view) moved to session_tools.py (controller) to respect MVC method

--- app/games/session/views.py
+++ app/games/session/views.py
@@ -7,7 +7,7 @@ from .models.forms import AddSession, SessionSearchForm
 import datetime
 from . import session as session_bp
 from app.models import Session, TimeSlot
-from .models.session_tools import TITLES, DEFAULT_TITLE, get_sessions_payload
+from .models.session_tools import TITLES, DEFAULT_TITLE, get_sessions_payload, add_session_form
 
 
 @session_bp.route('/sessions', methods=['GET', 'POST'])
@@ -24,6 +24,7 @@ def sessions():
     title = TITLES.get(payload.get("search_parameter"), DEFAULT_TITLE)
 
     return render_template('sessions.html', stylesheet='users', title=title, **payload)
+
 
 @session_bp.route('/session/<int:session_id>', methods=['GET', 'POST'])
 @login_required
@@ -66,55 +67,9 @@ def organize_session():
     form = AddSession()
 
     if request.method == "POST":
-        date = form.day.data
-        timeB = form.timeB.data
-        timeE = form.timeE.data
-
-        is_ok = True
-
-        print(date)
-        print(timeB)
-        print(timeE)
-
-        # check format day
-        if (date == None):
-            is_ok = False
-            flash("Le format de la date ne correspond pas à AAAA-MM-JJ et/ou le numéro du mois (1-12) et/ou du jour(1-29/30/31 selon le mois) est incorrecte", "warning")
-
-        # check format timeB
-        if (timeB == None):
-            is_ok = False
-            flash("Le format de l'heure de début ne correspond pas à HH:MM, et/ou le numéro de l'heure (0-23) et/ou le nombre de minutes (0-59) est incorecte", "warning")
-        
-        # check format timeE
-        if (timeE == None):
-            is_ok = False
-            flash("Le format de l'heure de fin ne correspond pas à HH:MM, et/ou le numéro de l'heure (0-23) et/ou le nombre de minutes (0-59) est incorecte", "warning")
-
-        if is_ok:
-            timeB.replace(second=0)
-            timeE.replace(second=0)
-
-            datetimeform = date.isoformat()+"T"+timeB.strftime('%H:%M:%S')
-
-            actual = datetime.datetime.now().replace(microsecond = 0).isoformat() #give a sting in ISO 8601 : "YYYY-MM-DDTHH:MM:SS"
-
-            if actual > datetimeform:
-                is_ok = False
-                flash("Vous ne pouvez pas créer une session pour une date antérieur à aujoud'hui", "warning")
-                return redirect(url_for('session.organize_session'))
-            
-            else: 
-                timeout = datetime.date(year=date.year, month=date.month, day=date.day+2)
-
-                timeslot = TimeSlot (timeB.strftime('%H:%M:%S'), timeE.strftime('%H:%M:%S'), date.isoformat())
-                # timeslot.add_to_db()
-                session = Session(0,timeout.strftime('%Y-%m-%d %H:%M:%S'),1)
-                # session.add_to_db()
-
-                return redirect(url_for('session.session',session_id=1))
+        if add_session_form(form):
+            return redirect(url_for('session.session',session_id=1))
         else:
             return redirect(url_for("session.organize_session"))
-    
     else:
         return render_template('organize_session.html', stylesheet='organize_session', form=form, dest="session.organize_session")
